94d456252e8e837fa6af75b1405cb506c31c792b
ndt07032004 <danhthai07032004ndt@gmail.com>
2025-10-28 16:33:52 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="digitaltour",
+    version="0.1.0",
+    author="PhamVanHuy",
+    author_email="huyveo37@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ src/prompt.py
@@ -0,0 +1,5 @@
+system_prompt = (
+  "Bạn là 1 bác sĩ . hãy nói chuyện thân thiện và tốt nhất"
+    "\n\n"
+    "{context}"
+)
